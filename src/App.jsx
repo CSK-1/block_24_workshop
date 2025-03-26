03c5cd06fa5e9fdf845a7787ce086f0184c02832
Chase K <chasekmbll@gmail.com>
2025-03-25 20:39:50 -0500

    more work

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
-import './App.css'
+import "./App.css";
+import { puppyList } from "./data.js";
+import { useState } from "react";
 
 function App() {
-  
-
-  return (
-    <>
-    </>
-  )
+	const [puppies, setPuppies] = useState(puppyList);
+	console.log(puppies);
+	return (
+		<div>
+			{
+        puppies.map((puppy) => {
+				  return <p key={puppy.id}>{puppy.name}</p>;
+			  })
+      }
+		</div>
+	);
 }
 
-export default App
+export default App;
